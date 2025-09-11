c38999b2825e3b21278010dd0d7978b471caa9f7
Marlon Malagkit <mrippimp@yahoo.com>
2025-09-11 23:59:28 +0800

    feat(app): add shadcn ui and clean supabase tutorial components

--- app/auth/sign-up-success/page.tsx
+++ app/auth/sign-up-success/page.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -23,6 +24,15 @@ export default function Page() {
                 You&apos;ve successfully signed up. Please check your email to
                 confirm your account before signing in.
               </p>
+              <div className="mt-4 text-center text-sm">
+                Please go back to{" "}
+                <Link
+                  href="/auth/login"
+                  className="underline underline-offset-4"
+                >
+                  Login
+                </Link>
+              </div>
             </CardContent>
           </Card>
         </div>
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -40,7 +40,7 @@ export function SignUpForm({
     }
 
     try {
-      const { error } = await supabase.auth.signUp({
+      const { data, error } = await supabase.auth.signUp({
         email,
         password,
         options: {
@@ -48,6 +48,19 @@ export function SignUpForm({
         },
       });
       if (error) throw error;
+
+      const user = data.user;
+
+      if (user) {
+        const { error } = await supabase.from("users").insert([
+          {
+            id: user.id,
+            email: user.email,
+          },
+        ]);
+        if (error) throw error;
+      }
+
       router.push("/auth/sign-up-success");
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : "An error occurred");
