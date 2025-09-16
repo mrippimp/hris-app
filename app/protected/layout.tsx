56234695ab064f712a15b28a447db207b9e9b1f9
Marlon Malagkit <mrippimp@yahoo.com>
2025-09-16 23:22:09 +0800

    feat(admin): add navbar component

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/layout/navbar";
-import Sidebar from "@/components/layout/sidebar";
+import AppSidebar from "@/components/layout/app-sidebar";
 import { ThemeSwitcher } from "@/components/theme-switcher";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function ProtectedLayout({
   children,
@@ -9,30 +10,32 @@ export default function ProtectedLayout({
 }) {
   return (
     <div className="flex">
-      <Sidebar />
-      <main className="min-h-screen flex flex-col items-center w-11/12">
-        <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="flex flex-col items-start w-11/12 min-h-screen">
           <Navbar />
-          <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-            {children}
-          </div>
+          <div className="flex flex-col items-center flex-1 w-full gap-20">
+            <div className="flex flex-col flex-1 max-w-5xl gap-20 p-5">
+              {children}
+            </div>
 
-          <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-            <p>
-              Powered by{" "}
-              <a
-                href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                target="_blank"
-                className="font-bold hover:underline"
-                rel="noreferrer"
-              >
-                Supabase
-              </a>
-            </p>
-            <ThemeSwitcher />
-          </footer>
-        </div>
-      </main>
+            <footer className="flex items-center justify-center w-full gap-8 py-16 mx-auto text-xs text-center border-t">
+              <p>
+                Powered by{" "}
+                <a
+                  href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+                  target="_blank"
+                  className="font-bold hover:underline"
+                  rel="noreferrer"
+                >
+                  Supabase
+                </a>
+              </p>
+              <ThemeSwitcher />
+            </footer>
+          </div>
+        </main>
+      </SidebarProvider>
     </div>
   );
 }
--- components/layout/sidebar.tsx
+++
@@ -1,5 +0,0 @@
-const Sidebar = () => {
-  return <div className="w-[15%]">Sidebar</div>;
-};
-
-export default Sidebar;
