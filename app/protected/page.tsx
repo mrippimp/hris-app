3f314650de9e151555ce9503fd8a5bb6abb6ff3c
Marlon Malagkit <mrippimp@yahoo.com>
2025-09-17 13:27:21 +0800

    feat(admin): add sidebar component and install dependencies

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
+import Image from "next/image";
 
 export default async function ProtectedPage() {
   const supabase = await createClient();
--- components/layout/app-sidebar.tsx
+++ components/layout/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Sidebar,
   SidebarContent,
@@ -9,9 +11,35 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarSeparator,
 } from "@/components/ui/sidebar";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import {
+  Calendar,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Inbox,
+  LogOut,
+  Search,
+  Settings,
+  User,
+  User2,
+} from "lucide-react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { DropdownMenuContent, DropdownMenuItem } from "../ui/dropdown-menu";
+import { Collapsible, CollapsibleTrigger } from "../ui/collapsible";
+import { CollapsibleContent } from "@radix-ui/react-collapsible";
 
 // Menu items.
 const items = [
@@ -37,35 +65,163 @@ const items = [
   },
   {
     title: "Settings",
-    url: "#",
     icon: Settings,
+    subMenus: [
+      {
+        title: "Profile",
+        url: "/settings/profile",
+        icon: User,
+      },
+      {
+        title: "Account",
+        url: "/settings/account",
+        icon: User2,
+      },
+    ],
   },
 ];
 
 const AppSidebar = () => {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme } = useTheme();
+
+  // resolve hydration using resolvedTheme
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
   return (
-    <Sidebar>
-      <SidebarHeader>HRIS Logo</SidebarHeader>
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="py-4">
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton>
+              <Link href="/">
+                <Image
+                  src={
+                    resolvedTheme === "dark"
+                      ? "/HorizonHR_Light.png"
+                      : "/HorizonHR_Dark.png"
+                  }
+                  alt="default logo"
+                  width={40}
+                  height={40}
+                />
+                <span className="sr-only">HRIS Logo</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarSeparator />
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
+              {/* {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton>
+                  <SidebarMenuButton asChild>
                     <Link href={item.url}>
                       <item.icon />
                       {item.title}
                     </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
-              ))}
+              ))} */}
+              {items.map((item) =>
+                item.subMenus ? (
+                  // <Collapsible className="group/collapsible">
+                  //   <SidebarGroup>
+                  //     <SidebarGroupLabel asChild>
+                  //       <CollapsibleTrigger>
+                  //         {item.title}{" "}
+                  //         <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                  //       </CollapsibleTrigger>
+                  //     </SidebarGroupLabel>
+                  //     <CollapsibleContent>
+                  //       {item.subMenus?.map((subMenu) => (
+                  //         <SidebarMenuItem key={subMenu.title}>
+                  //           <SidebarMenuButton asChild>
+                  //             <Link href={subMenu.url}>
+                  //               <subMenu.icon />
+                  //               {subMenu.title}
+                  //             </Link>
+                  //           </SidebarMenuButton>
+                  //         </SidebarMenuItem>
+                  //       ))}
+                  //     </CollapsibleContent>
+                  //   </SidebarGroup>
+                  // </Collapsible>
+                  <Collapsible className="group/collapsible">
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <CollapsibleTrigger>
+                          <item.icon />
+                          {item.title}
+                          <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        </CollapsibleTrigger>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <CollapsibleContent>
+                      {item.subMenus?.map((subMenu) => (
+                        <SidebarMenuSub>
+                          <SidebarMenuSubItem key={subMenu.title}>
+                            <SidebarMenuSubButton asChild>
+                              <Link href={subMenu.url}>
+                                <subMenu.icon />
+                                {subMenu.title}
+                              </Link>
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        </SidebarMenuSub>
+                      ))}
+                    </CollapsibleContent>
+                  </Collapsible>
+                ) : (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link href={item.url}>
+                        <item.icon />
+                        {item.title}
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                )
+              )}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>Footer</SidebarFooter>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <Avatar className="w-7 h-7">
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>...</AvatarFallback>
+                  </Avatar>
+                  John Doe
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <User /> Profile
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Settings /> Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <LogOut /> Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
